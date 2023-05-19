472385f3ae08fa26d23f6f6cdc6f9a3cd6ce1cc0
Md-Tur <turibnmostafiz@gmail.com>
2023-05-19 10:14:23 +0600

    resume and experience updated

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -4,7 +4,20 @@ export const experienceData = [
         company: 'Non Academy',
         jobtitle: 'React.JS Developer',
         startYear: 'October, 2022',
-        endYear: 'Present'
+        endYear: 'November, 2022'
     },
-
+    {
+        id: 2,
+        company: 'ITCO',
+        jobtitle: 'MERN Stack (Front End) Developer',
+        startYear: 'December, 2022',
+        endYear: 'February, 2023'
+    },
+    {
+        id: 3,
+        company: 'Pi Technology BD',
+        jobtitle: 'Front End Developer',
+        startYear: 'April, 2023',
+        endYear: 'Present'
+    }
 ]
